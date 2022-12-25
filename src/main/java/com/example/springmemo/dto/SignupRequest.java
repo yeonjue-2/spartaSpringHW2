83ed720592f556b45672e649603b5dc9511e4c82
wwwndus <simock22@gmail.com>
2022-12-26 01:45:28 +0900

    refactor: 정규식 수정

--- src/main/java/com/example/springmemo/dto/SignupRequest.java
+++ src/main/java/com/example/springmemo/dto/SignupRequest.java
@@ -10,13 +10,14 @@ import javax.validation.constraints.Size;
 @Setter
 public class SignupRequest {
 
-    @Pattern(regexp = "[a-z0-9]*$")
+    @Pattern(regexp = "^[a-z0-9]*$")
     @Size(min = 4, max = 10, message = "이름은 4 ~ 10자이어야 합니다.")
     private String username;
 
-    @Pattern(regexp = "[a-zA-Z0-9]*$")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     @Size(min = 8, max = 15, message = "비밀번호는 8 ~ 15자이어야 합니다!")
     private String password;
+
     private boolean admin = false;
     private String adminToken = "";
 }
